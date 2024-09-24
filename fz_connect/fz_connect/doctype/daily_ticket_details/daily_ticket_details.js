5d6272a65cbb3afdcc4867a36dce826eca288d18
Adimyra Systems Private Limited <adimyra@frappe-dev.us-south1-c.c.concrete-bloom-432215-i7.internal>
2024-09-24 10:10:16 +0000

    Doctype update

---
+++ fz_connect/fz_connect/doctype/daily_ticket_details/daily_ticket_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Faiyaz and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Daily Ticket Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
